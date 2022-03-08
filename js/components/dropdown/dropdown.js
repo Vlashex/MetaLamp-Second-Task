583a022a5e516608bfa045971bd0b01d442cce61
vlad <rmainavine@gmail.com>
2022-03-08 09:21:36 +0300

    Попробовал переделать dropdown через callback, внес правки в структуру html

--- js/components/dropdown/dropdown.js
+++ js/components/dropdown/dropdown.js
@@ -8,8 +8,9 @@ for (let i = 0; i < DropDown.length; i++) {
   const dropdownButCount = element.querySelectorAll('.dropdown__button-count-btn');
   const dropdownMenu = element.querySelectorAll('.dropdown__menu');
   let dropdownAllCount = 0;
-  element.addEventListener('click', (event) => {
-    if (event.target === dropdownArrow) {
+
+  function ArrowFunc(target){
+    if (target === dropdownArrow) {
       dropdownArrow.classList.toggle('form__button-active');
       if (dropdown.style.display === '') {
         dropdown.style.display = 'none';
@@ -19,19 +20,25 @@ for (let i = 0; i < DropDown.length; i++) {
         formIn.style.borderRadius = '4px 4px 0 0';
       }
     }
+  }
+
+  element.addEventListener('click', (event)=>{
+    ArrowFunc(event.target)
   });
+
   for (let i = 0; i < dropdownMenu.length; i++) {
     const element = dropdownMenu[i];
     let dropdownOut = element.querySelector('.dropdown__out');
-    element.addEventListener('click', (event) => {
+
+    function DropDownFunc(target){
       let dropdownUp = element.querySelector('.dropdownUp');
       let dropdownDown = element.querySelector('.dropdownDown');
       let dropdownOutCount = dropdownOut.innerHTML;
-      if (event.target === dropdownUp) {
+      if (target === dropdownUp) {
         dropdownOutCount++
         dropdownAllCount++
         dropdownOut.innerHTML = dropdownOutCount;
-      } else if (event.target === dropdownDown && dropdownOutCount > 0) {
+      } else if (target === dropdownDown && dropdownOutCount > 0) {
         dropdownOutCount--
         dropdownAllCount--
         if (dropdownAllCount < 0) {
@@ -44,18 +51,31 @@ for (let i = 0; i < DropDown.length; i++) {
       } else {
         dropdownButCount[0].style.opacity = '0';
       }
+    }
+
+    element.addEventListener('click', (event) => {
+      DropDownFunc(event.target)
     });
+
+
     dropdownButCount[0].addEventListener('click', () => {
       dropdownAllCount = 0;
       formIn.placeholder = 'Сколько гостей';
-      dropdownOut.innerHTML = 0;
+      formIn.value ='';
+      dropdownOut.innerHTML = '0';
+      dropdownButCount[0].style.opacity = '0';
     });
+
+
     dropdownButCount[1].addEventListener('click', () => {
       formIn.placeholder = ''
       formIn.value = dropdownAllCount;
       if (dropdownAllCount === 0) {
         formIn.placeholder = 'Сколько гостей';
+        formIn.value =''
       }
     });
+
+
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import "../style/css/style.scss"
-import "./components/jsx/App.jsx"
 import "../style/css/main.css"
 import "../style/css/normalize.css"
+
+
+import './components/dropdown/dropdown'
